08da6db33822ea6d982d964bda9de7253093bd78
ncymic <98546379+ncymic@users.noreply.github.com>
2022-01-28 01:15:25 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,148 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 11,
+   "id": "7bac030a-7386-438e-81d8-4085d8d847a8",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "7"
+      ]
+     },
+     "execution_count": 11,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "3+4"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 10,
+   "id": "d0967321-b408-4a70-854c-77932aae1979",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "2"
+      ]
+     },
+     "execution_count": 10,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "1+1"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "963b2aad-2885-4cb2-a8c5-f57f3529b370",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "markdown",
+   "id": "85aaf5f2-3b86-49c0-adf2-64d29a7c4d18",
+   "metadata": {},
+   "source": [
+    "My Title"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "7717e632-0b1f-4bdc-803d-ccd9304c4f5c",
+   "metadata": {
+    "tags": []
+   },
+   "source": [
+    "## My Title"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "29d4c2ae-c5d0-4fac-8232-05f26dc3def4",
+   "metadata": {},
+   "source": [
+    "<a href=\"https://cognitiveclass.ai/?utm_medium=Exinfluencer&#x26;utm_source=Exinfluencer&#x26;utm_content=000026UJ&#x26;utm_term=10006555&#x26;utm_id=NA-SkillsNetwork-Channel-SkillsNetworkCoursesIBMDeveloperSkillsNetworkDS0105ENSkillsNetwork20083975-2021-01-01\">Cognitive Class</a>"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "36ebf9a0-2335-46af-bb54-7c3f8ffaf82b",
+   "metadata": {},
+   "source": [
+    "# Header 1"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "723d64df-32f5-4b1c-b994-e5090e6eebfe",
+   "metadata": {},
+   "source": [
+    "### Header 3"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "57f4a64f-a386-40d3-990e-be8fc6757fac",
+   "metadata": {},
+   "source": [
+    "|header|header|\n",
+    "|------|------|\n",
+    "|cell|cell|"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "f15e5960-fb3f-447c-8f2f-eae60ac36090",
+   "metadata": {},
+   "source": [
+    "|col1|col2|col3|\n",
+    "|----|----|----|\n",
+    "|row1|row1|row1|\n",
+    "|row2|row2|row2|\n",
+    "|row3|row3|row3|\n",
+    "|row4|row4|row4|"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "24e503be-3fbc-4f8f-ba58-523c3cb0565d",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python",
+   "language": "python",
+   "name": "conda-env-python-py"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.12"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
